733a3b72827b679b576e3a28e77343113b90d45e
KattyGZC <kattyko.gzc@gmail.com>
2026-02-09 17:35:58 -0500

    Alembic y docker config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/f1db"
+    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5435/f1db"
     OPENF1_BASE_URL: str = "https://api.openf1.org/v1"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
